c81f771cda31e91a5c9ea5ebbd7a54330fb05412
marialekseeva <catastroficmaker@gmail.com>
2020-03-12 23:39:27 +0500

    add hometask for step 6.3 (Page Object)

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,20 @@
+import self as self
+from selenium.common.exceptions import NoSuchElementException
+from urllib3.util import timeout
+
+
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)   #команда для неявного ожидания, значение по умолчанию 10
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
