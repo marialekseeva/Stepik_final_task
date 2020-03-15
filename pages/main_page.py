d4779d09234675a5fe8bc5a2977be23a500b4fe3
marialekseeva <catastroficmaker@gmail.com>
2020-03-15 21:47:55 +0500

    feat(page object): add transitions between pages

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,16 +1,15 @@
 from .base_page import BasePage
-
 from selenium.webdriver.common.by import By
 from .locators import MainPageLocators
-
+from .login_page import LoginPage
 from .base_page import BasePage
 
 
 class MainPage(BasePage):
 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
 
     def should_be_login_link(self):
-        self.browser.current_url
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
